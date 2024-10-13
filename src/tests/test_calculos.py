4bb9658ed6a15540244210242861d83a9aa48919
Joaquin Bejar <jb@taunais.com>
2024-10-13 20:22:23 +0200

    Change literal types to Enums for clarity and safety
    Replaced Literal types with Enum classes for 'Sexo' and 'ObjetivoNutricional' in key functions. This enhances type safety and readability, ensuring that only valid values are used for gender and nutritional goals while preventing invalid input.

--- src/tests/test_calculos.py
+++ src/tests/test_calculos.py
@@ -1,26 +1,27 @@
 import unittest
 
+from src.body_analyzer.model import ObjetivoNutricional
 from src.body_analyzer.calculos import calcular_macronutrientes
 
 
 class TestCalcularMacronutrientes(unittest.TestCase):
 
     def test_mantener_peso(self):
-        resultado = calcular_macronutrientes(2000, 'mantener peso')
+        resultado = calcular_macronutrientes(2000, ObjetivoNutricional.MANTENER_PESO)
         esperado = (150.0, 200.0, 66.67)  # Aproximado
         self.assertAlmostEqual(resultado[0], esperado[0], places=2)
         self.assertAlmostEqual(resultado[1], esperado[1], places=2)
         self.assertAlmostEqual(resultado[2], esperado[2], places=2)
 
     def test_perder_grasa(self):
-        resultado = calcular_macronutrientes(2000, 'perder grasa')
+        resultado = calcular_macronutrientes(2000, ObjetivoNutricional.PERDER_GRASA)
         esperado = (200.0, 200.0, 44.44)  # Aproximado
         self.assertAlmostEqual(resultado[0], esperado[0], places=2)
         self.assertAlmostEqual(resultado[1], esperado[1], places=2)
         self.assertAlmostEqual(resultado[2], esperado[2], places=2)
 
     def test_ganar_masa_muscular(self):
-        resultado = calcular_macronutrientes(2000, 'ganar masa muscular')
+        resultado = calcular_macronutrientes(2000, ObjetivoNutricional.GANAR_MASA_MUSCULAR)
         esperado = (150.0, 250.0, 44.44)  # Aproximado
         self.assertAlmostEqual(resultado[0], esperado[0], places=2)
         self.assertAlmostEqual(resultado[1], esperado[1], places=2)
