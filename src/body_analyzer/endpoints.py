08b7f87b3c9c45d070e8752b8abfb6363189ae07
pablotech80 <ptecherasosa@icloud.com>
2024-10-22 18:56:10 +0200

    Descripción de los cambios

--- src/body_analyzer/endpoints.py
+++ src/body_analyzer/endpoints.py
@@ -1,8 +1,16 @@
 from flask import jsonify, request
 
+from src.body_analyzer.interpretaciones import (
+    interpretar_ffmi,
+    interpretar_imc,
+    interpretar_porcentaje_grasa,
+    interpretar_ratio_cintura_altura,
+    interpretar_rcc,
+)
+from src.body_analyzer.model import Sexo
+
 from .calculos import *
 from .constantes import *
-from .model import Sexo
 
 
 def configure_routes(app):
@@ -674,5 +682,108 @@ def configure_routes(app):
             return jsonify({"error": f"Error de valor: {str(e)}"}), 400
         except TypeError as e:
             return jsonify({"error": f"Error de tipo: {str(e)}"}), 400
+        except Exception as e:
+            return jsonify({"error": f"Error interno del servidor: {str(e)}"}), 500
+
+    @app.route("/informe_completo", methods=["POST"])
+    def informe_completo_endpoint():
+        """
+        Interpreta y devuelve un informe con todos los resultados del análisis de composición corporal.
+        :return: Informe completo
+        """
+        try:
+            # Obtener datos necesarios
+            data = request.get_json()
+            peso = data.get("peso")
+            altura = data.get("altura")
+            edad = data.get("edad")
+            genero = data.get("genero")
+            cuello = data.get("cuello")
+            cintura = data.get("cintura")
+            cadera = data.get("cadera")
+
+            # Validación de parámetros obligatorios
+            if None in (peso, altura, edad, genero, cuello, cintura):
+                return jsonify({"error": "Faltan parámetros obligatorios"}), 400
+
+            # Validación de tipos de datos
+            if not isinstance(peso, (int, float)) or peso <= 0:
+                return jsonify({"error": "El peso debe ser un número positivo"}), 400
+            if not isinstance(altura, (int, float)) or altura <= 0:
+                return jsonify({"error": "La altura debe ser un número positivo"}), 400
+            if not isinstance(edad, int) or edad <= 0:
+                return (
+                    jsonify({"error": "La edad debe ser un número entero positivo"}),
+                    400,
+                )
+            if genero not in ["h", "m"]:
+                return (
+                    jsonify({"error": "El valor de 'genero' debe ser 'h' o 'm'"}),
+                    400,
+                )
+
+            # Convertir genero a Enum Sexo
+            genero_enum = Sexo.HOMBRE if genero == "h" else Sexo.MUJER
+
+            # Realizar cálculos
+            porcentaje_grasa = calcular_porcentaje_grasa(
+                cintura, cuello, altura, genero_enum, cadera
+            )
+            tmb = calcular_tmb(peso, altura, edad, genero_enum)
+            imc = calcular_imc(peso, altura)
+            masa_muscular = peso - (peso * (porcentaje_grasa / 100))
+            agua_total = calcular_agua_total(peso, altura, edad, genero_enum)
+            ffmi = calcular_ffmi(masa_muscular, altura)
+            peso_saludable_min, peso_saludable_max = calcular_peso_saludable(altura)
+            sobrepeso = calcular_sobrepeso(peso, altura)
+            rcc = calcular_rcc(cintura, cadera) if genero_enum == Sexo.MUJER else "N/A"
+            ratio_cintura_altura = calcular_ratio_cintura_altura(cintura, altura)
+
+            # Realizar interpretaciones
+            interpretacion_imc = interpretar_imc(imc, ffmi, genero_enum)
+            interpretacion_grasa = interpretar_porcentaje_grasa(
+                porcentaje_grasa, genero_enum
+            )
+            interpretacion_ffmi = interpretar_ffmi(ffmi, genero_enum)
+            interpretacion_rcc = (
+                interpretar_rcc(rcc, genero_enum)
+                if genero_enum == Sexo.MUJER
+                else "N/A"
+            )
+            interpretacion_ratio_cintura_altura = interpretar_ratio_cintura_altura(
+                ratio_cintura_altura
+            )
+
+            # Consolidar el informe completo en resultado e interpretaciones, en un diccionario
+            resultados = {
+                "tmb": tmb,
+                "imc": imc,
+                "porcentaje_grasa": porcentaje_grasa,
+                "masa_muscular": masa_muscular,
+                "agua_total": agua_total,
+                "ffmi": ffmi,
+                "peso_saludable": {
+                    "min": peso_saludable_min,
+                    "max": peso_saludable_max,
+                },
+                "sobrepeso": sobrepeso,
+                "rcc": rcc,
+                "ratio_cintura_altura": ratio_cintura_altura,
+            }
+
+            interpretaciones = {
+                "imc": interpretacion_imc,
+                "porcentaje_grasa": interpretacion_grasa,
+                "ffmi": interpretacion_ffmi,
+                "rcc": interpretacion_rcc,
+                "ratio_cintura_altura": interpretacion_ratio_cintura_altura,
+            }
+
+            informe = {"resultados": resultados, "interpretaciones": interpretaciones}
+
+            return jsonify(informe), 200
+
+        except ValueError as e:
+            return jsonify({"error": str(e)}), 400
         except Exception as e:
             return jsonify({"error": f"Error interno del servidor: {str(e)}"}), 500
--- src/body_analyzer/main.py
+++ src/body_analyzer/main.py
@@ -1,17 +1,16 @@
-from flask import Flask
+import flask
 
+from src.body_analyzer.constantes import *
 from src.body_analyzer.endpoints import configure_routes
 
-app = Flask(__name__)
-
-for rule in app.url_map.iter_rules():
-    print(rule)
+app = flask.Flask(__name__)
 
 # Llama a la función para configurar las rutas
 configure_routes(app)
 
 # Imprimir las rutas registradas
-print(app.url_map)
+for rule in app.url_map.iter_rules():
+    print(rule)
 
 if __name__ == "__main__":
     app.run(debug=True)
