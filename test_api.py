447ba1e2e3a27df42a4c54ee8bcfa1bff1cab705
Pablo Techera <ptecherasosa@icloud.com>
2024-11-10 18:31:15 +0100

    Add test_api.py to local branch

---
+++ test_api.py
@@ -0,0 +1,19 @@
+import requests as rq
+import json
+
+url = "http://127.0.0.1:5000/informe_completo"
+
+data = {
+    "peso": 70,
+    "altura": 170,
+    "edad": 30,
+    "genero": "m",
+    "cuello": 35,
+    "cintura": 75,
+    "cadera": 95,
+    "objetivo": "perder grasa",  # Cambiamos según el objetivo que quieras probar
+}
+
+response = rq.post(url, json=data)
+formatted_json = json.dumps(response.json(), indent=4, ensure_ascii=False)
+print(formatted_json)
